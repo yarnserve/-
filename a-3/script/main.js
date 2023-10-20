eb4d356613b1d6e635547f9abef286fa951c1297
yarnserve <yarnserve@gmail.com>
2023-10-20 14:43:51 +0900

    init

---
+++ a-3/script/main.js
@@ -0,0 +1,41 @@
+$(function(){   
+    // 메뉴 
+    $('.gnb > li').hover(
+        function(){
+            $(this).find('ul').stop().slideDown()
+        },
+        function(){
+            $(this).find('ul').stop().slideUp()
+        },
+    )
+
+    // 슬라이드
+    setInterval(function(){
+        $('.slides').animate({top: '-100%'}, function(){
+            $('.slide').first().appendTo('.slides')
+            $(this).css('top', 0)
+        })
+    }, 3000)
+
+    // 탭
+    $('.item').eq(0).show()
+
+    $('.titles h3').click(function(){            
+        var idx =  $(this).index()
+        
+        $('.titles h3').removeClass('active')
+        $(this).addClass('active')
+
+        $('.item').hide()
+        $('.item').eq(idx).show()       
+    })
+    
+    // 모달
+    $('#showModal').click(function(){
+        $('.modal').show()
+    })
+    
+    $('#closeModal').click(function(){
+        $('.modal').hide()
+    })    
+})
